7a17a22cf39e888bc49866b35e9f086ec1de82ad
AmbroseByamugisha <ambrosebyamugisha@gmail.com>
2019-07-22 17:01:39 +0300

    updated the nav bar

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,14 @@ import React, { Component } from 'react';
 class Navbar extends Component {
     render() {
         return(
-            <div>Hello there!</div>
+            <div>
+                <ul className="NavBar">
+                    <li className="nav-item"><a class="active" href="#home">Home</a></li>
+                    <li className="nav-item"><a href="#news">News</a></li>
+                    <li className="nav-item"><a href="#contact">Contact</a></li>
+                    <li className="nav-item nav-item-right"><a href="#about">About</a></li>
+                </ul>
+            </div>
         )
     }
 };
